8a17aaef54fef0e2e56265f097c83386e2cebe5d
Craig Cairns <cairns.craig@gmail.com>
2016-03-16 07:39:54 -0600

    Adding generated code

--- src/components/Annotation/Annotation.js
+++ src/components/Annotation/Annotation.js
@@ -1,12 +1,42 @@
 import React, { Component, PropTypes } from 'react';
 import ProtoBuf from 'protobufjs';
+import paper, { Tool, Path } from 'paper';
 
 class Annotation extends Component {
+  componentDidMount() {
+    // Create an empty project and a view for the canvas:
+    paper.setup(this.refs.canvas);
+    var tool = new Tool();
+    var path;
+
+    // Define a mousedown and mousedrag handler
+    tool.onMouseDown = function(event) {
+      path = new Path();
+      path.strokeColor = 'black';
+      path.add(event.point);
+    }
+
+    tool.onMouseDrag = function(event) {
+      console.log(event);
+      path.add(event.point);
+    }
+  }
+
   render() {
+    // Scale the drawing area to match DPI.
+    let width = '500px'
+    let height = '500px';
+    let style = {
+      width: this.props.width,
+      height: this.props.height
+    };
+
     return (
-      <div>
-        <h2>YO YO YO</h2>
-      </div>
+      React.createElement('canvas', {
+        ref: 'canvas',
+        width: width,
+        height: height,
+        style: style })
     );
   }
 
